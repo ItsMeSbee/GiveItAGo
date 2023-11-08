73ec79ed481395943f613b6b7e2bac9a81d36e6f
Jude <jude.snee@gmail.com>
2023-11-08 13:08:14 +1300

    beep beoop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("beep beghkfghkeopHejfgjgfjllo world!");
+        System.out.println("Hello world!");
     }
 }
